a9478950d952efd4f6d8971ce725fe207c5480fb
LaupWing <loc_strict@hotmail.com>
2023-03-04 09:48:47 +0100

    udpate

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,5 @@
-<h1><?php echo $heading; ?></h1>
-<?php foreach($listings as $listing): ?>
-   <h1><?php echo $listing["title"]; ?></h1>
-   <p><?php echo $listing["description"]; ?></p>
-<?php endforeach; ?>
+<h1>{{$heading}}</h1>
+@foreach($listings as $listing)
+   <h1>{{$listing["title"]}}</h1>
+   <p>{{$listing["description"]}}</p>
+@endforeach
