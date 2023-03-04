a68ce567f1754e575134b991a0ba1c613a1a6783
LaupWing <loc_strict@hotmail.com>
2023-03-04 09:50:27 +0100

    no listings

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,4 +1,9 @@
 <h1>{{$heading}}</h1>
+
+@if(count($listings) == 0)
+   <p>No listings found</p>
+@endif
+
 @foreach($listings as $listing)
    <h1>{{$listing["title"]}}</h1>
    <p>{{$listing["description"]}}</p>
