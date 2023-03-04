b796b51a63b0434a7fdaebb8f23bc771211d55f6
LaupWing <loc_strict@hotmail.com>
2023-03-04 10:03:58 +0100

    okaay

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::get("/", function () {
    return view("listings", [
       "heading" => "Latest Listings",
-      "listings" => [
-         [
-            "id" => 1,
-            "title" => "Listing One",
-            "description" => "Lorem ipsum"
-         ],
-         [
-            "id" => 2,
-            "title" => "Listing Two",
-            "description" => "Lorem ipsum"
-         ],
-      ]
+      "listings" => Listing::all()
+   ]);
+});
+
+
+Route::get("/listings/{id}", function($id){
+   return view("listing", [
+      "listing" => Listing::find($id)
    ]);
 });
