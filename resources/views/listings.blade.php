12d937547892654e3472b1c4a7c83c45f1a19ee5
LaupWing <loc_strict@hotmail.com>
2023-03-04 10:20:49 +0100

    udpate

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -5,6 +5,10 @@
 @endif
 
 @foreach($listings as $listing)
-   <h1>{{$listing["title"]}}</h1>
+   <h2>
+      <a href="/listings/{{$listing['id']}}">
+         {{$listing["title"]}}
+      </a>
+   </h2>
    <p>{{$listing["description"]}}</p>
 @endforeach
