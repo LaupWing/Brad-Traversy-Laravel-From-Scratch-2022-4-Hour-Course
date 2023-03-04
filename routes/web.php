a912d074ad875b62858a31cf54ba3ff1b12e3e8c
LaupWing <loc_strict@hotmail.com>
2023-03-04 09:33:34 +0100

    udpate

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-   return view('welcome');
+Route::get("/", function () {
+   return view("welcome");
 });
+
+
+Route::get("/hello", function () {
+   return response("<h1>Hello World </h1>", 200)
+      ->header("Content-Type", "text/plain")
+      ->header("foo", "bar");
+});
+
+Route::get("/post/{id}", function($id){
+   dd($id);
+   return response("Post ". $id);
+})->where("id", "[0-9]+");
