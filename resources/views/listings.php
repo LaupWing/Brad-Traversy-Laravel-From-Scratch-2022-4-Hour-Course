4a69afeeba1c64edddffe7e3de09418a3346ce2c
LaupWing <loc_strict@hotmail.com>
2023-03-04 09:46:24 +0100

    udpate

--- resources/views/listings.php
+++ resources/views/listings.php
@@ -1,1 +1,5 @@
 <h1><?php echo $heading; ?></h1>
+<?php foreach($listings as $listing): ?>
+   <h1><?php echo $listing["title"]; ?></h1>
+   <p><?php echo $listing["description"]; ?></p>
+<?php endforeach; ?>
