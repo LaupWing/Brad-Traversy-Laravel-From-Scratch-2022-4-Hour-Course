affce13dd3cadd1b37fe0b44647f940676059f09
LaupWing <loc_strict@hotmail.com>
2023-03-06 20:01:38 +0100

    update

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -6,7 +6,7 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-   <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+   <x-card class="p-24">
       <div class="flex flex-col items-center justify-center text-center">
          <img
             class="w-48 mr-6 mb-6"
@@ -32,29 +32,29 @@
          <div class="text-lg my-4">
             <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
          </div>
-         <div class="border border-gray-200 w-full mb-6"></div>
+         <div class="border border-gray-200 w-full mb-6" />
          <div>
-         <h3 class="text-3xl font-bold mb-4">
-            Job Description
-         </h3>
-         <div class="text-lg space-y-6">
-            {{$listing->description}}
-            <a
-               href="mailto:{{$listing->email}}"
-               class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
-               ><i class="fa-solid fa-envelope"></i>
-               Contact Employer</a
-            >
-
-            <a
-               href="{{$listing->website}}"
-               target="_blank"
-               class="block bg-black text-white py-2 rounded-xl hover:opacity-80"
-               ><i class="fa-solid fa-globe"></i> Visit
-               Website</a
-            >
+            <h3 class="text-3xl font-bold mb-4">
+               Job Description
+            </h3>
+            <div class="text-lg space-y-6">
+               {{$listing->description}}
+               <a
+                  href="mailto:{{$listing->email}}"
+                  class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
+                  ><i class="fa-solid fa-envelope"></i>
+                  Contact Employer</a
+               >
+               <a
+                  href="{{$listing->website}}"
+                  target="_blank"
+                  class="block bg-black text-white py-2 rounded-xl hover:opacity-80"
+                  ><i class="fa-solid fa-globe"></i> Visit
+                  Website</a
+               >
+            </div>
          </div>
-      </div>
-   </div>
+      <div>
+   </x-card>
 </div>
 @endsection
