4105f2c63c274244e4ba75b57d8631430fc0a2e7
LaupWing <loc_strict@hotmail.com>
2023-03-14 06:06:08 +0100

    update

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,10 @@
 @if(session()->has("message"))
-   <div class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-40">
+   <div 
+      x-data="{show: true}" 
+      x-init="setTimeout(()=> show = false, 3000)"
+      x-show="show"
+      class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-40"
+   >
       <p>
          {{session("message")}}
       </p>
