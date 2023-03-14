44f110cb16bba560a309c7ca03af3476b5584f40
LaupWing <loc_strict@hotmail.com>
2023-03-14 05:34:19 +0100

    udpate

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -8,117 +8,118 @@
          <p class="mb-4">Post a gig to find a developer</p>
       </header>
 
-      <form action="">
+      <form method="POST" action="/listings">
+         @csrf
          <div class="mb-6">
-               <label
-                  for="company"
-                  class="inline-block text-lg mb-2"
-                  >Company Name</label
-               >
-               <input
-                  type="text"
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="company"
-               />
+            <label
+               for="company"
+               class="inline-block text-lg mb-2"
+               >Company Name</label
+            >
+            <input
+               type="text"
+               class="border border-gray-200 rounded p-2 w-full"
+               name="company"
+            />
          </div>
 
          <div class="mb-6">
-               <label for="title" class="inline-block text-lg mb-2"
-                  >Job Title</label
-               >
-               <input
-                  type="text"
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="title"
-                  placeholder="Example: Senior Laravel Developer"
-               />
+            <label for="title" class="inline-block text-lg mb-2"
+               >Job Title</label
+            >
+            <input
+               type="text"
+               class="border border-gray-200 rounded p-2 w-full"
+               name="title"
+               placeholder="Example: Senior Laravel Developer"
+            />
          </div>
 
          <div class="mb-6">
-               <label
-                  for="location"
-                  class="inline-block text-lg mb-2"
-                  >Job Location</label
-               >
-               <input
-                  type="text"
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="location"
-                  placeholder="Example: Remote, Boston MA, etc"
-               />
+            <label
+               for="location"
+               class="inline-block text-lg mb-2"
+               >Job Location</label
+            >
+            <input
+               type="text"
+               class="border border-gray-200 rounded p-2 w-full"
+               name="location"
+               placeholder="Example: Remote, Boston MA, etc"
+            />
          </div>
 
          <div class="mb-6">
-               <label for="email" class="inline-block text-lg mb-2"
-                  >Contact Email</label
-               >
-               <input
-                  type="text"
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="email"
-               />
+            <label for="email" class="inline-block text-lg mb-2"
+               >Contact Email</label
+            >
+            <input
+               type="text"
+               class="border border-gray-200 rounded p-2 w-full"
+               name="email"
+            />
          </div>
 
          <div class="mb-6">
-               <label
-                  for="website"
-                  class="inline-block text-lg mb-2"
-               >
-                  Website/Application URL
-               </label>
-               <input
-                  type="text"
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="website"
-               />
+            <label
+               for="website"
+               class="inline-block text-lg mb-2"
+            >
+               Website/Application URL
+            </label>
+            <input
+               type="text"
+               class="border border-gray-200 rounded p-2 w-full"
+               name="website"
+            />
          </div>
 
          <div class="mb-6">
-               <label for="tags" class="inline-block text-lg mb-2">
-                  Tags (Comma Separated)
-               </label>
-               <input
-                  type="text"
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="tags"
-                  placeholder="Example: Laravel, Backend, Postgres, etc"
-               />
+            <label for="tags" class="inline-block text-lg mb-2">
+               Tags (Comma Separated)
+            </label>
+            <input
+               type="text"
+               class="border border-gray-200 rounded p-2 w-full"
+               name="tags"
+               placeholder="Example: Laravel, Backend, Postgres, etc"
+            />
          </div>
 
          <div class="mb-6">
-               <label for="logo" class="inline-block text-lg mb-2">
-                  Company Logo
-               </label>
-               <input
-                  type="file"
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="logo"
-               />
+            <label for="logo" class="inline-block text-lg mb-2">
+               Company Logo
+            </label>
+            <input
+               type="file"
+               class="border border-gray-200 rounded p-2 w-full"
+               name="logo"
+            />
          </div>
 
          <div class="mb-6">
-               <label
-                  for="description"
-                  class="inline-block text-lg mb-2"
-               >
-                  Job Description
-               </label>
-               <textarea
-                  class="border border-gray-200 rounded p-2 w-full"
-                  name="description"
-                  rows="10"
-                  placeholder="Include tasks, requirements, salary, etc"
-               ></textarea>
+            <label
+               for="description"
+               class="inline-block text-lg mb-2"
+            >
+               Job Description
+            </label>
+            <textarea
+               class="border border-gray-200 rounded p-2 w-full"
+               name="description"
+               rows="10"
+               placeholder="Include tasks, requirements, salary, etc"
+            ></textarea>
          </div>
 
          <div class="mb-6">
-               <button
-                  class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
-               >
-                  Create Gig
-               </button>
+            <button
+               class="bg-laravel text-white rounded py-2 px-4 hover:bg-black"
+            >
+               Create Gig
+            </button>
 
-               <a href="/" class="text-black ml-4"> Back </a>
+            <a href="/" class="text-black ml-4"> Back </a>
          </div>
       </form>
    </x-card>
