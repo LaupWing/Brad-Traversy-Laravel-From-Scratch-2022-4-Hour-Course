06b91e4a62d8e1bf811d388432d380ea6cadcb44
LaupWing <loc_strict@hotmail.com>
2023-03-17 05:45:00 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,3 +38,6 @@ Route::delete("/listings/{listing}", [ListingController::class, "destroy"]);
 
 // Show single listing
 Route::get("/listings/{listing}", [ListingController::class, "show"]);
+
+// Show register/create form
+Route::get("/register", [UserController::class, "create"]);
